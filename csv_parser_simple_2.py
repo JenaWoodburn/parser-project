fd98e8c79768e22066739595c4034277c2741936
Jena Woodburn <jena.woodburn@gmail.com>
2025-06-05 22:08:32 +0200

    Add more specific IOError checks and os.path.exists checks for files

--- csv_parser_simple_2.py
+++ csv_parser_simple_2.py
@@ -1,25 +1,48 @@
 #simple file parsers
+import os
 
 #make a copy of a file 
 try:
-    with open("data/constituents_short.csv", "rt") as filein, open("data/out/constituents_short_copy.csv", "w") as fileout:
-        contents = filein.read()
-        fileout.write(contents)
+    filein = open("data/constituents_short.csv", "rt") 
 except FileNotFoundError:
-    print("File does not exist")
+    print(f"Input file does not exist")
+    exit()
 except IOError as e:
     print(f"An I/O Error occurred: {e}")
+    exit()
+    
+try:
+    fileout = open("data/out/constituents_short_copy.csv", "w") 
+except FileNotFoundError:
+    print(f"Output file path is invalid")
+    exit()
+except IOError as e:
+    print(f"An I/O Error occurred: {e}")
+    exit()
+
+with filein, fileout:   
+    try:
+        contents = filein.read()
+        fileout.write(contents)
+    except IOError as e:
+        print(f"An error occurred: {e}")
 
 
 #make a copy of a file, contents transformed to all uppercase 
-try:
-    with open ("data/constituents_short.csv", "rt") as filein, open("data/out/constituents_short_upper.csv", "w") as fileout:
-        contents = filein.read()
-        fileout.write(contents.upper())
-except FileNotFoundError:
-    print("File does not exist")
-except IOError as e:
-    print(f"An I/O Error occurred: {e}")
+filein = "data/constituents_short.csv"
+fileout = "data/out/constituents_short_upper.csv"
+
+if not os.path.exists(filein):
+    print(f"Input file does not exist: {filein}")
+elif not os.path.exists(os.path.dirname(fileout)):
+    print(f"Output directory does not exist: {os.path.dirname(fileout)}")
+else:
+    try:
+        with open (filein, "rt") as filein, open(fileout, "w") as fileout:
+            contents = filein.read()
+            fileout.write(contents.upper())
+    except IOError as e:
+        print(f"An error occurred: {e}")
 
 
 #pretty print file contents with headers in format:
@@ -27,51 +50,89 @@ except IOError as e:
     # Name: Agilent Technologies
     # Sector: Life Sciences Tools & Services
 #creates new file
-try:
-    with open("data/constituents_short.csv", "rt") as filein, open("data/out/constituents_short_pp.csv", "w") as fileout:
-        #get header elements
-        header_cols = filein.readline().rsplit(",")
-        col1 = header_cols[0]
-        col2 = header_cols[1]
-        col3 = header_cols[2].rstrip()
+filein = "data/constituents_short.csv"
+fileout = "data/out/constituents_short_pp.csv"
 
-        # #pretty print contents
-        contents = filein.readlines()
-        for row in contents:
-            elements = row.rsplit(",")
-            fileout.write((f'{col1}: {elements[0]}\n'))
-            fileout.write((f'{col2}: {elements[1]}\n'))
-            #rejoin any elements split on ',' inside "", remove ""
-            sector = (",".join(elements[2:])).strip('"\n')
-            fileout.write((f'{col3}: {sector}\n\n'))
-except FileNotFoundError:
-    print("File does not exist")
-except IOError as e:
-    print(f"An I/O Error occurred: {e}")
+if not os.path.exists(filein):
+    print(f"Input file does not exist: {filein}")
+elif not os.path.exists(os.path.dirname(fileout)):
+    print(f"Output directory does not exist {fileout}")
+else:
+    try:
+        with open(filein, "rt") as filein, open(fileout, "w") as fileout:
+            #get header elements
+            header_cols = filein.readline().rsplit(",")
+            col1 = header_cols[0]
+            col2 = header_cols[1]
+            col3 = header_cols[2].rstrip()
+
+            # #pretty print contents
+            contents = filein.readlines()
+            for row in contents:
+                elements = row.rsplit(",")
+                fileout.write((f'{col1}: {elements[0]}\n'))
+                fileout.write((f'{col2}: {elements[1]}\n'))
+                #rejoin any elements split on ',' inside "", remove ""
+                sector = (",".join(elements[2:])).strip('"\n')
+                fileout.write((f'{col3}: {sector}\n\n'))
+    except IOError as e:
+        print(f"An I/O Error occurred: {e}")
 
 
 #append as set of data to an existing file
 
 # #create file by copying 'constituents_short.csv'
 try:
-    with open("data/constituents_short.csv", "rt") as filein, open("data/out/constituents_short_A-B.csv", "w") as fileout:
+    filein = open("data/constituents_short.csv", "rt")
+except FileNotFoundError:
+    print("Input file does not exist")
+    exit()
+except IOError as e:
+    print(f"An I/O Error occurred: {e}")
+    exit()
+
+try:
+    fileout = open("data/out/constituents_short_A-B.csv", "w")
+except FileNotFoundError:
+    print("Output directory does not exist")
+    exit()
+except IOError as e:
+    print(f"An I/O Error occurred: {e}")
+    exit()
+    
+with filein, fileout:
+    try:
         contents = filein.read()
         fileout.write(contents)
         fileout.write("\n")
-except FileNotFoundError:
-    print("File does not exist")
-except IOError as e:
-    print(f"An I/O Error occurred: {e}")
+    except IOError as e:
+        print(f"An I/O Error occurred: {e}")
 
 
 #append all companies beginning with 'B' to file
 try:
-    with open("data/constituents.csv", "rt") as filein, open("data/out/constituents_short_A-B.csv", "a") as fileout:        
+    filein = open("data/constituents.csv")
+except FileNotFoundError:
+    print("Input file does not exist")
+    exit()
+except IOError as e:
+    print(f"An I/O Error occurred")
+    exit()
+
+try:
+    fileout = open("data/out/constituents_short_A-B.csv", "a")
+except FileNotFoundError:
+    print("Output directory does not exist")
+    exit()
+except IOError as e:
+    print(f"An I/O Error occurred")
+    exit()
+
+with filein, fileout:        
+    try:
         contents = filein.readlines()
         for line in contents:
             if line.startswith("B"):
                 fileout.write(line)
-except FileNotFoundError:
-    print("File does not exist")
-except IOError as e:
-    print(f"An I/O Error occurred: {e}")
+    except IOError as e:
+        print(f"An I/O Error occurred: {e}")
